39a7d27148fbab55c31db6f9ccea98e8ec31871d
Marvellousiscool <adebayoinioluwamarvellous1@gmail.com>
2023-12-31 19:46:56 +0100

    modified votepage

--- votingbash/routes/clientRoutes/voteNow.js
+++ votingbash/routes/clientRoutes/voteNow.js
@@ -1,47 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const clientController = require("../../controllers/clientController");
-const got = require("got");
-const connection = require("../../models/connection"); // Import your MySQL connection
+const connection = require("../../models/connection");
 
-// Replace with your actual Flutterwave Secret Key
-const flutterwaveSecretKey = "FLWSECK_TEST-2dce25fd88f38b28cc8998a2da505355-X";
+// Replace with your Paystack Secret Key
+const paystackSecretKey = process.env.PST_SECRETKEY;
+const paystack = require("paystack")(paystackSecretKey); // Explicitly require Paystack module
+
+// Function to retrieve selected contestant based on nickname
+async function getSelectedContestant(nickname) {
+  return await clientController.getContestantByNickname(nickname);
+}
 
 router.post("/:nickname/votenow/payment/process", async (req, res) => {
   try {
     const nickname = req.params.nickname;
-    const selectedContestant = await clientController.getContestantByNickname(
-      nickname
-    );
+    const selectedContestant = await getSelectedContestant(nickname);
     const { email } = req.body;
 
-    // Construct Flutterwave payment details
-    const flutterwavePaymentDetails = {
-      tx_ref: `vote_${selectedContestant.nickname}_${Date.now()}`,
-      amount: "50",
-      currency: "NGN",
-      redirect_url: `/contestant/${selectedContestant.nickname}/votenow/payment/callback`,
-      meta: {
-        consumer_id: Math.floor(Math.random() * 1000) + 1,
-        consumer_mac: generateRandomMacAddress(),
-      },
-      customer: {
-        email: email,
-      },
-      customizations: {
-        title: `Payment for ${selectedContestant.nickname}'s vote`,
-      },
+    // Construct Paystack transaction details
+    const paystackTransactionDetails = {
+      email,
+      amount: 5000, // Replace with your desired amount in kobo (e.g., 5000 for ₦50.00)
+      reference: `vote_${selectedContestant.nickname}_${Date.now()}`.replace(
+        /[^\w_]/g,
+        ""
+      ), // Remove invalid characters
     };
 
-    // Make a POST request to Flutterwave API using 'got'
-    const response = await got
-      .post("https://api.flutterwave.com/v3/payments", {
-        headers: {
-          Authorization: `Bearer ${flutterwaveSecretKey}`,
-        },
-        json: flutterwavePaymentDetails,
-      })
-      .json();
+    // Make a POST request to Paystack API to initialize the transaction
+    const response = await paystack.transaction.initialize(
+      paystackTransactionDetails
+    );
 
     // Save the payment transaction details to the database
     const savePaymentQuery = `
@@ -50,66 +40,35 @@ router.post("/:nickname/votenow/payment/process", async (req, res) => {
     `;
     await connection.query(savePaymentQuery, [
       selectedContestant.id,
-      response.data.tx_ref,
-      flutterwavePaymentDetails.amount,
-      flutterwavePaymentDetails.currency,
-      "pending", // You may set the initial status as "pending"
+      response.data.reference,
+      paystackTransactionDetails.amount,
+      "NGN", // Assuming the currency is NGN
+      "pending",
     ]);
 
-    // Redirect the user to the payment link
-    res.redirect(response.data.link);
+    // Send the authorization URL to the client
+    res.json({ authorization_url: response.data.authorization_url });
   } catch (error) {
     // Handle errors
     console.error("Error processing payment:", error);
-    if (error.response) {
-      console.error("Flutterwave API Error Response:", error.response.body);
-      console.error("Status Code:", error.response.statusCode);
-      console.error("Headers:", error.response.headers);
-    } else if (error.request) {
-      console.error("No response received from Flutterwave API");
-      console.error("Request details:", error.request.options);
-    } else {
-      console.error("Error setting up the request:", error.message);
-    }
-    res.status(500).send("Internal Server Error");
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-function generateRandomMacAddress() {
-  const characters = "0123456789ABCDEF";
-  let macAddress = "";
-
-  for (let i = 0; i < 12; i++) {
-    if (i > 0 && i % 2 === 0) {
-      macAddress += "-";
-    }
-
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    macAddress += characters[randomIndex];
-  }
-
-  return macAddress;
-}
-
 router.get("/:nickname/votenow/payment/callback", async (req, res) => {
   try {
-    const transactionReference = req.query.transaction_id;
+    console.log("Paystack Callback Request Received:", req.query);
+    const nickname = req.params.nickname;
+    const selectedContestant = await getSelectedContestant(nickname);
 
-    // Verify the Flutterwave transaction
-    const response = await got
-      .get(
-        `https://api.flutterwave.com/v3/transactions/${transactionReference}/verify`,
-        {
-          headers: {
-            Authorization: `Bearer ${flutterwaveSecretKey}`,
-          },
-        }
-      )
-      .json();
+    const transactionReference = req.query.reference;
+
+    // Verify the Paystack transaction
+    const response = await paystack.transaction.verify(transactionReference);
 
     // Check if the payment was successful
     if (
-      response.data.status === "successful" &&
+      response.data.status === "success" &&
       response.data.currency === "NGN"
     ) {
       // Update the payment status in the database
@@ -126,13 +85,13 @@ router.get("/:nickname/votenow/payment/callback", async (req, res) => {
 
       // Proceed with your logic based on the payment details
       if (paymentDetails.length > 0) {
-        res.render("voteNowSuccess", {
+        return res.render("voteNowSuccess", {
           selectedContestant,
           paymentSuccess: true,
         });
       } else {
         // Handle the case where payment details are not found
-        res.status(400).send("Invalid payment details");
+        return res.status(400).send("Invalid payment details");
       }
     } else {
       // Update the payment status in the database
@@ -141,14 +100,14 @@ router.get("/:nickname/votenow/payment/callback", async (req, res) => {
       await connection.query(updatePaymentQuery, [transactionReference]);
 
       // Inform the customer that their payment was unsuccessful
-      res.render("voteNowSuccess", {
+      return res.render("voteNowSuccess", {
         selectedContestant,
         paymentSuccess: false,
       });
     }
   } catch (error) {
-    console.error("Error processing Flutterwave callback:", error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error processing Paystack callback:", error);
+    return res.status(500).send("Internal Server Error");
   }
 });
 
