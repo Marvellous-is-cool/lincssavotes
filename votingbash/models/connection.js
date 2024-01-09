c345a4cc983badf0814b6e46e6cc7f1e970fbb19
Marvellousiscool <adebayoinioluwamarvellous1@gmail.com>
2024-01-09 01:54:50 +0100

    modified connection

--- votingbash/models/connection.js
+++ votingbash/models/connection.js
@@ -4,27 +4,27 @@ const mysql = require("mysql2");
 // const dbPassword = process.env.DB_PASSWORD || "2SbPcToA";
 
 // Create the connection pool to the database
-// const pool = mysql.createPool({
-//   host: "68.64.164.95", // Database Host
-//   user: "admin", // Database User
-//   password: "5ceh0w5a", // Use the environment variable or default to "L1NCSSABASH"
-//   database: "bashvote", // Database Name
-//   port: 10011, // Database Port
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
 const pool = mysql.createPool({
-  host: "localhost", // Database Host
-  user: "root", // Database User
-  password: "", // Use the environment variable or default to "L1NCSSABASH"
+  host: "68.64.164.95", // Database Host
+  user: "admin", // Database User
+  password: "5ceh0w5a", // Use the environment variable or default to "L1NCSSABASH"
   database: "bashvote", // Database Name
-  port: 3306, // Database Port
+  port: 10011, // Database Port
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+// const pool = mysql.createPool({
+//   host: "localhost", // Database Host
+//   user: "root", // Database User
+//   password: "", // Use the environment variable or default to "L1NCSSABASH"
+//   database: "bashvote", // Database Name
+//   port: 3306, // Database Port
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 // Export the promise-based interface of the pool
 const connection = pool.promise();
