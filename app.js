859ea3270e200223eb36cdcbc9d3f050bdeb3b06
Marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-07-08 12:03:54 +0100

    lincssa votes17

--- app.js
+++ app.js
@@ -63,7 +63,10 @@ app.use((req, res, next) => {
     console.log(`Static file request: ${req.method} ${req.url}`);
   }
   next();
-}); => {
+});
+
+// Add validator to res.locals
+app.use((req, res, next) => {
   res.locals.validator = validator;
   next();
 });
