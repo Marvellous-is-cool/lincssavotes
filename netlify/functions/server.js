c3976a542b77ecbd02f3fdabfe8a7f2c0485000b
Marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2025-07-08 11:36:09 +0100

    lincssa votes14

--- netlify/functions/server.js
+++
@@ -1,7 +0,0 @@
-const serverless = require("serverless-http");
-
-// Import the Express app
-const app = require("../../app");
-
-// Wrap the Express app with serverless-http
-exports.handler = serverless(app);
